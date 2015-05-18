6b9b5693432d8ac4be4a26a6e10d420865185088
tetiana.filonenko <tetiana.filonenko2012@gmail.com>
2015-05-19 01:20:02 +0300

    add blob

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../boot', __FILE__)
-
+require "azure"
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,7 +11,8 @@ module RorAzureDemo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    Azure.config.storage_account_name = "rubyrtest"
+    Azure.config.storage_access_key = "ws9iUa/PWA7lbyAHhMVZJJosFnfQ1QbpUWfzjyoN9FwFeDDPrz303PJhrNRHFIVU/D1yZK8GfQHKl2LWwVsTzg=="
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
